6609a1df6d17a02d559279deede0360c54608f95
Mohamed9669 <kingmetzo1@gmail.com>
2025-09-13 21:09:16 +0000

    quelques_amelioration

--- src/app/api/task/all/route.ts
+++ src/app/api/task/all/route.ts
@@ -4,11 +4,17 @@ import { NextResponse } from "next/server";
 
 export const GET = async (request: Request) => {
   try {
-    await connectToDB;
-    const tasks = await Task.find({});
+    await connectToDB();
+    const tasks = await Task.find();
     return NextResponse.json(tasks, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json("Failed to fetch all tasks", { status: 500 });
   }
 };
+
+// export async function GET() {
+//   await connectToDB();
+//   const tasks = await Task.find();
+//   return NextResponse.json(tasks);
+// }
